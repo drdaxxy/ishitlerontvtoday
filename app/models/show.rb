a99f1335aae2b51c5882ba9b64a62f6f02d6ae48
niklas <niklas@spiderschwe.in>
2014-12-09 12:34:25 +0100

    fix and improve Show model

--- app/models/show.rb
+++ app/models/show.rb
@@ -14,12 +14,15 @@ class Show
 	
 	has n, :tags, :through => Resource
 	
-	validates_presence_of :channel, :unless => lambda { |o| o.relevant? }
+	validates_presence_of :channel, :if => lambda { |o| o.relevant? }
 	validates_with_block { @starts_at < @ends_at }
-	# no overlaps
-	validates_with_block { count(:starts_at.lt => @ends_at, :ends_at.gt => @starts_at) == 0 }
+	
+	# no overlaps, but allow updates of unchecked shows
+	before :save do |show|
+		Show.all(:starts_at.lt => show.ends_at, :ends_at.gt => show.starts_at, :channel_dvbid => show.channel_dvbid, :relevant => nil).destroy
+	end
 	
 	def self.today
-		all(:starts_at.lt => (Date.today + 1).to_time, :ends_at.gt => (Date.today - 1).to_time)
+		all(:starts_at.lt => (Date.today + 1).to_time, :ends_at.gt => (Date.today).to_time - 1)
 	end
 end
