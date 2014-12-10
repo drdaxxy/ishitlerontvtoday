88f82bebb41223640e9b611ca9c482f5dcd7f865
niklas <niklas@spiderschwe.in>
2014-12-10 23:19:37 +0100

    adjust filters

--- app/routes/admin.rb
+++ app/routes/admin.rb
@@ -3,7 +3,8 @@ class IsHitlerOnTvToday
 	get '/admin' do
 		@shows = Show.in_text("Hitler") \
 			+ Show.in_text("Nazi") \
-			+ Show.in_text("National") \
+			+ Show.in_text("Sozialis") \
+			+ Show.in_text("Socialis") \
 			+ Show.in_text("Himmler") \
 			+ Show.in_text("Goebbels") \
 			+ Show.in_text("Weltkrieg") \
@@ -14,7 +15,8 @@ class IsHitlerOnTvToday
 			+ Show.in_text("Drittes Reich") \
 			+ Show.in_text("Dritte Reich") \
 			+ Show.in_text("Dritten Reich") \
-			+ Show.in_text("NSDAP")
+			+ Show.in_text("NSDAP") \
+			+ Show.in_text("NPD")
 	end
 
 end
