93ec545c556d1019db8bf19f8ef04c83e9978c55
niklas <niklas@spiderschwe.in>
2014-12-11 18:01:38 +0100

    admin interface is getting there...

--- app/routes/admin.rb
+++ app/routes/admin.rb
@@ -1,24 +1,31 @@
 class IsHitlerOnTvToday
 
-	get '/admin' do
-		@shows = Show.in_text("Hitler") \
-			+ Show.in_text("Nazi") \
-			+ Show.in_text("Sozialis") \
-			+ Show.in_text("Socialis") \
-			+ Show.in_text("Nationalis") \
-			+ Show.in_text("Rechtsextrem") \
-			+ Show.in_text("Himmler") \
-			+ Show.in_text("Goebbels") \
-			+ Show.in_text("Weltkrieg") \
-			+ Show.in_text("World War") \
-			+ Show.in_text("Alliierte") \
-			+ Show.in_text("Allied") \
-			+ Show.in_text("Third Reich") \
-			+ Show.in_text("Drittes Reich") \
-			+ Show.in_text("Dritte Reich") \
-			+ Show.in_text("Dritten Reich") \
-			+ Show.in_text("NSDAP") \
-			+ Show.in_text("NPD")
+	get '/admin/' do
+		# don't want these to be lazily loaded
+		@dvbchannels = Dvbchannel.all.reload
+		
+		# we have to order them first
+		all_shows = Show.all(:order => [:starts_at.asc])
+		@shows = all_shows.in_text("Hitler") \
+			+ all_shows.in_text("Nazi") \
+			+ all_shows.in_text("Sozialis") \
+			+ all_shows.in_text("Socialis") \
+			+ all_shows.in_text("Nationalis") \
+			+ all_shows.in_text("Rechtsextrem") \
+			+ all_shows.in_text("Himmler") \
+			+ all_shows.in_text("Goebbels") \
+			+ all_shows.in_text("Weltkrieg") \
+			+ all_shows.in_text("World War") \
+			+ all_shows.in_text("Alliierte") \
+			+ all_shows.in_text("Allied") \
+			+ all_shows.in_text("Third Reich") \
+			+ all_shows.in_text("Drittes Reich") \
+			+ all_shows.in_text("Dritte Reich") \
+			+ all_shows.in_text("Dritten Reich") \
+			+ all_shows.in_text("NSDAP") \
+			+ all_shows.in_text("NPD")
+		
+		erb :admin
 	end
 
 end
