1533abcf620f22de1b226613c0d09b66e8d74557
niklas <niklas@spiderschwe.in>
2014-12-11 16:47:30 +0100

    Permalink preparation

--- app/routes/index.rb
+++ app/routes/index.rb
@@ -1,6 +1,16 @@
 class IsHitlerOnTvToday
-	get '/' do
-		@shows = Show.today.all(:relevant => true)
+	get '/?:date?' do
+		if params[:date].nil?
+			@shows = Show.today.all(:relevant => true)
+		else
+			begin
+				@date = Date.strptime(params[:date], "%F")
+				throw ArgumentError if @date > Date.today
+			rescue ArgumentError # invalid or future date
+				redirect to("/")
+			end
+			@shows = Show.on_day(@date).all(:relevant => true)
+		end
 		
 		erb :index
 	end
